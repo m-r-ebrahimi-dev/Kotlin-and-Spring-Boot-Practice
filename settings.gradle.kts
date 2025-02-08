289aae4787b9d0c11cb3cc430d8485ae3e271c04
mohammadreza <mrebrahimidev@gmail.com>
2025-02-08 14:43:40 +0330

    initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "m-r-ebrahimi-dev"
---
+++ src/main/kotlin/com/github/mrebrahimidev/KotlinSpringExerciseApplication.kt
@@ -0,0 +1,11 @@
+package com.github.mrebrahimidev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringExerciseApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringExerciseApplication>(*args)
+}
